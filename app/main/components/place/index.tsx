3258e7bbdaa7c449a69d931cc0b40e1fd11bbcd9
abdullah harits <harits.abdullah19@gmail.com>
2023-11-08 05:53:59 +0700

    implement detail location

--- app/main/components/place/index.tsx
+++ app/main/components/place/index.tsx
@@ -9,43 +9,75 @@ import { kText } from "@/constans/text";
 
 export default function Place() {
   return (
-    <Scaffold className="">
-      <TopDecor />
-      <Background className="-scale-y-100 -z-50" />
-      <Background className="-scale-y-100 -z-50" />
-      <div className="flex flex-col items-center justify-center text-center  left-0 right-0 px-10">
-        <Text family="berkshire" className="text-3xl">
-          {kText.saveTheDate}
-        </Text>
-        <br />
-        <div className="h-11" />
-        <Text family="averia" className="text-base">
-          {kText.theWedding}
-        </Text>
-        <Text family="averia" className="text-2xl">
-          <DateText />
-        </Text>
-        <div className="h-1" />
-        <Text family="averia" className="text-base">
-          {kText.pukul} {kText.waktuPernikahan}
-        </Text>
-        <div className="h-1" />
-        <Text family="averia" className="text-sm">
-          {kText.venue}
-        </Text>
-        <div className="h-4" />
-        <button className="px-4 py-2 bg-ae814c text-white rounded-full">
-          {kText.mapLokasiAcara}
-        </button>
-        <br /> <br /> <br />
-        <CountDown />
-        <div className="h-4" />
-        <Text>
-          {kText.merupakanSuatuKehormatan}
-          {kText.memberikanDoaRestu}
-        </Text>
-      </div>
-      <BottomDecor />
-    </Scaffold>
+    <div>
+      <Scaffold className="">
+        <TopDecor />
+        <Background className="-scale-y-100 -z-50" />
+        <Background className="-scale-y-100 -z-50" />
+        <div className="flex flex-col items-center justify-center text-center  left-0 right-0 px-10">
+          <Text family="berkshire" className="text-3xl">
+            {kText.saveTheDate}
+          </Text>
+          <br />
+          <div className="h-11" />
+          <Text family="averia" className="text-base">
+            {kText.theWedding}
+          </Text>
+          <Text family="averia" className="text-2xl">
+            <DateText />
+          </Text>
+          <div className="h-1" />
+          <Text family="averia" className="text-base">
+            {kText.pukul} {kText.waktuPernikahan}
+          </Text>
+          <div className="h-1" />
+          <Text family="averia" className="text-sm">
+            {kText.venue}
+          </Text>
+          <div className="h-4" />
+          <button
+            className="px-4 py-2 bg-ae814c text-white rounded-full"
+            onClick={() => {
+              window.open(kText.linkMaps, "_blank");
+            }}
+          >
+            {kText.mapLokasiAcara}
+          </button>
+          <br /> <br /> <br />
+          <CountDown />
+          <div className="h-4" />
+          <Text>
+            {kText.merupakanSuatuKehormatan}
+            {kText.memberikanDoaRestu}
+          </Text>
+        </div>
+        <BottomDecor />
+      </Scaffold>
+      <Scaffold className="">
+        <TopDecor />
+        <Background className="-z-50" />
+
+        <div className="px-11 whitespace-pre-line text-center my-6">
+          <Detail
+            title={kText.acaraAkad}
+            desc={`${kText.hariAcaraAkad}\n${kText.pukul} ${kText.waktuAcaraAkad}`}
+          />
+          <Detail
+            title={kText.acaraResepsi}
+            desc={`${kText.hariAcaraResepsi}\n${kText.pukul} ${kText.waktuAcaraResepsi}`}
+          />
+        </div>
+        <BottomDecor />
+      </Scaffold>
+    </div>
   );
 }
+
+const Detail = (props: { title: string; desc: string }) => {
+  return (
+    <>
+      <Text className="font-semibold text-base">{props.title}</Text>
+      <Text>{props.desc}</Text>
+    </>
+  );
+};
--- constans/text.ts
+++ constans/text.ts
@@ -11,24 +11,35 @@ export const kText = {
     "وَمِنْ اٰيٰتِهٖٓ اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ اَزْوَاجًا لِّتَسْكُنُوْٓا اِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَّوَدَّةً وَّرَحْمَةً ۗاِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ يَّتَفَكَّرُوْنَ",
   ayat1Arti: `"Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir."`,
   ayat1Surat: "(QS Ar-Rum: 21)",
-  namaPengantin1: 'Muhammad Fulan Madani, S.Pi.',
-  ayahPengantin1: 'Nama Ayah Pengantin 1',
-  ibuPengantin1: 'Nama Ibu Pengantin 1',
-  anakKePengantin1: 'Pertama',
-  alamatPengantin1: 'Beralamat di Plosokuning III RT 13/05, Minomartani, Ngaglik, Sleman',
+  namaPengantin1: "Muhammad Fulan Madani, S.Pi.",
+  ayahPengantin1: "Nama Ayah Pengantin 1",
+  ibuPengantin1: "Nama Ibu Pengantin 1",
+  anakKePengantin1: "Pertama",
+  alamatPengantin1:
+    "Beralamat di Plosokuning III RT 13/05, Minomartani, Ngaglik, Sleman",
   namaPengantin2: "Aisyah Fulan Fulanah, S.Pd.",
-  ayahPengantin2: 'Fulan',
-  ibuPengantin2: 'Fulanah',
-  alamatPengantin2: 'Beralamat di Plosokuning III RT 13/05, Minomartani, Ngaglik, Sleman',
-  anakKePengantin2: 'Kedua',
+  ayahPengantin2: "Fulan",
+  ibuPengantin2: "Fulanah",
+  alamatPengantin2:
+    "Beralamat di Plosokuning III RT 13/05, Minomartani, Ngaglik, Sleman",
+  anakKePengantin2: "Kedua",
   saveTheDate: "Save The Date",
   waktuPernikahan: "11:30 WIB - 12:30 WIB",
   pukul: "Pukul",
   venue: "Gedung Serbaguna Kalitirto",
   mapLokasiAcara: "Map Lokasi Acara",
-  merupakanSuatuKehormatan: "Merupakan suatu kehormatan dan kebahagiaan bagi kami sekeluarga, apabila Bapak/Ibu/Saudara/i ​​berkenan hadir dan memberikan doa restu. ",
-  memberikanDoaRestu: "memberikan doa restu. Atas kehadiran dan doa restunya, kami mengucapkan terima kasih.",
-
-
+  merupakanSuatuKehormatan:
+    "Merupakan suatu kehormatan dan kebahagiaan bagi kami sekeluarga, apabila Bapak/Ibu/Saudara/i ​​berkenan hadir dan memberikan doa restu. ",
+  memberikanDoaRestu:
+    "memberikan doa restu. Atas kehadiran dan doa restunya, kami mengucapkan terima kasih.",
+  linkMaps: "https://maps.app.goo.gl/ZsafwexTP5KwTCr3A",
+  acaraAkad: "Acara Akad",
+  hariAcaraAkad: "Minggu, 15 Oktober 2023",
+  waktuAcaraAkad: "09.00 - 09.30 WIB",
+  tempatAcaraAkad: "Gedung Serbaguna Kalitirto",
+  acaraResepsi: "Acara Resepsi",
+  hariAcaraResepsi: "Minggu, 15 Oktober 2023",
+  waktuAcaraResepsi: "09.00 - 09.30 WIB",
+  tempatAcaraResepsi: "Gedung Serbaguna Kalitirto",
 
 };

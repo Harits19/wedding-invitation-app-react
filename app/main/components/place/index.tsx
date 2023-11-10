beed6ae5632805c7e1cc49f7ab8b62be195a6a22
Abdullah Harits <harits.abdullah19@gmail.com>
2023-11-10 15:08:48 +0700

    remove unused code, add config

--- app/main/components/place/index.tsx
+++ app/main/components/place/index.tsx
@@ -2,13 +2,7 @@ import useToQuery from "@/app/hooks/useToQuery";
 import Location from "../location";
 import DetailLocation from "../detail-location";
 import Attendance from "../attendance";
-import { kText } from "@/constans/text";
 import Greeting from "../greeting";
-import Scaffold from "@/app/components/scaffold";
-import TextDiv from "@/app/components/averia";
-import Input from "@/app/components/input";
-import TextArea from "@/app/components/textarea";
-import Select from "@/app/components/select";
 
 export default function Place() {
   const name = useToQuery();

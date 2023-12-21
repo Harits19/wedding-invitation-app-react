9225c6ec9e176c0cb8a22adac03718fdcdc10769
Abdullah Harits <harits.abdullah19@gmail.com>
2023-12-21 12:04:45 +0700

    wip, implement update wedding

--- app/api/wedding/[id]/route.ts
+++ app/api/wedding/[id]/route.ts
@@ -2,6 +2,11 @@ import { ResponseUtil } from "@/app/utils/response-util";
 import { checkApiKey } from "../route";
 import { weddingRepository } from "@/app/dependency";
 import { HttpStatusCode } from "axios";
+import {
+  WeddingModel,
+  weddingSchemaCreate,
+  weddingSchemaUpdate,
+} from "@/app/model/wedding-model";
 
 export const GET = async (
   req: Request,
@@ -12,7 +17,7 @@ export const GET = async (
     if (response) {
       return response;
     }
-    const result= await weddingRepository.getDetailWedding(id);
+    const result = await weddingRepository.getDetailWedding(id);
     if (result) {
       result.password = undefined;
       return ResponseUtil.success({
@@ -23,13 +28,45 @@ export const GET = async (
       });
     } else {
       return ResponseUtil.error({
-        code: HttpStatusCode.NotFound, 
+        code: HttpStatusCode.NotFound,
         payload: {
           message: "empty result",
-        
         },
       });
     }
+  } catch (error) {
+    return ResponseUtil.error({
+      payload: {
+        message: "error patch wedding",
+        error: error,
+      },
+    });
+  }
+};
+
+export const PATCH = async (
+  req: Request,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const body = {
+      ...(await req.json()),
+      id,
+    };
+    const validatedBody = await weddingSchemaUpdate.validate(body, {
+      abortEarly: false,
+    });
+
+    console.log('patch wedding body',)
+
+    await checkApiKey();
+    await weddingRepository.update(validatedBody);
+
+    return ResponseUtil.success({
+      payload: {
+        message: "success patch wedding",
+      },
+    });
   } catch (error) {
     return ResponseUtil.error({
       payload: {

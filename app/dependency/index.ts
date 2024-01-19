859209d25730b835d2ce1652ea8f15cf1293ab50
Abdullah Harits <harits.abdullah19@gmail.com>
2024-01-19 14:35:37 +0700

    remove vercel app

--- app/dependency/index.ts
+++ app/dependency/index.ts
@@ -1,9 +1,12 @@
 import { DatabaseModel } from "../model";
 import { GreetingRepositoryHandler } from "../repository/greeting-repository";
 import { WeddingRepositoryHandler } from "../repository/wedding-repository";
-import { createKysely } from "@vercel/postgres-kysely";
+// import { createKysely } from "@vercel/postgres-kysely";
 
-const vercelDb = createKysely<DatabaseModel>();
+// const vercelDb = createKysely<DatabaseModel>();
 
-export const weddingRepository = new WeddingRepositoryHandler({ vercelDb });
-export const greetingRepository = new GreetingRepositoryHandler({ vercelDb });
+// export const weddingRepository = new WeddingRepositoryHandler({ vercelDb });
+// export const greetingRepository = new GreetingRepositoryHandler({ vercelDb });
+
+export const weddingRepository: any = {};
+export const greetingRepository: any = {};

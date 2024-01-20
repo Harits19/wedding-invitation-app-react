c9be1928d9f91041caebbd5af4c423ce4c353834
abdullah harits <harits.abdullah19@gmail.com>
2024-01-20 15:50:11 +0700

    change password

--- app/config/mysql.ts
+++ app/config/mysql.ts
@@ -6,7 +6,7 @@ const mysql2Config = {
   database: "abdullah28_invitation",
   host: "103.58.102.50",
   user: "abdullah28_invitation",
-  password: "harits963741852",
+  password: "Prambanan1",
   port: 3306,
   connectTimeout: 1000000,
 };

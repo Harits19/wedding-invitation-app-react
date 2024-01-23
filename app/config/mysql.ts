e1e21e74bff3989d0aaceb6e32aee0ba980161a7
Abdullah Harits <harits.abdullah19@gmail.com>
2024-01-23 12:05:20 +0700

    delete password

--- app/config/mysql.ts
+++ app/config/mysql.ts
@@ -5,7 +5,7 @@ export const mysql2Config = {
   database: "abdullah28_invitation",
   host: "103.58.102.50",
   user: "abdullah28_invitation",
-  password: "harits963741852",
+  password: "-",
   port: 3306,
   connectTimeout: 1000000,
 };
---
+++ app/dependency/index.ts
@@ -0,0 +1,3 @@
+
+export const weddingRepository: any = {};
+export const greetingRepository: any = {};

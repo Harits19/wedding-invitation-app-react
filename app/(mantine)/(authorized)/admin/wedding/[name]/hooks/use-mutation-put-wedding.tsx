e600e03586c8afb31c932b7f35536c11b0033be3
Abdullah Harits <harits.abdullah19@gmail.com>
2024-03-04 21:07:59 +0700

    remove mantine ui

--- app/(mantine)/(authorized)/admin/wedding/[name]/hooks/use-mutation-put-wedding.tsx
+++
@@ -1,5 +0,0 @@
-import { useMutation } from "react-query";
-
-export const useMutationPutWedding = () => {
-  return useMutation({});
-};
--- app/(mantine)/(authorized)/admin/wedding/create/hooks/use-mutation-add-wedding.tsx
+++
@@ -1,8 +0,0 @@
-import { useFetchPostWedding } from "@/app/services/use-fetch-post-wedding";
-import { useMutation } from "react-query";
-
-export default function useMutationAddWedding() {
-  return useMutation({
-    mutationFn: useFetchPostWedding,
-  });
-}

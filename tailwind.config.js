aca38914e84c508e47d31e6238b9c6b7c417db98
Abdullah Harits <harits.abdullah19@gmail.com>
2024-03-07 14:42:41 +0700

    setup shacdn ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export const kColor = {
   303333: "#303333",
   driftwood: "#ae814c",
 };
-
 module.exports = {
+  darkMode: ["class"],
   content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,9 +17,16 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      colors: kColor,
       maxWidth: {
         360: "360px",
       },
@@ -25,7 +36,56 @@ module.exports = {
       height: {
         100: "100px",
       },
+      colors: {
+        ...kColor,
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
         "wiggle-left": {
           "0%": { transform: "rotate(0deg) scale(1)" },
           "50%": { transform: "rotate(4deg) scale(1.18)" },
@@ -78,8 +138,12 @@ module.exports = {
             opacity: "1",
           },
         },
+
       },
       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+
         "wiggle-left": "wiggle-left 3s ease-in-out infinite",
         "wiggle-right": "wiggle-right 3s ease-in-out infinite",
         "fade-zoom-out": "fade-zoom-out 5s linear forwards",
@@ -89,11 +153,12 @@ module.exports = {
         "bottom-top": "bottom-top 2s ease-in-out",
         "bottom-top-1s": "bottom-top 1s ease-in-out",
         "top-bottom-fade": "top-bottom-fade 2s",
+
       },
       backgroundImage: {
         "background-picture-1": `url(/background-picture-1.webp)`,
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}

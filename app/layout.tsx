cc9ba30ea7ad8530d98869a5569f8f895953da23
Abdullah Harits <harits.abdullah19@gmail.com>
2024-04-16 15:09:15 +0700

    update script

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body>
         <QueryProvider>
+          test update
           <Div>{children}</Div>
         </QueryProvider>
       </body>
